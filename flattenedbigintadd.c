60c5a1bcad7c12aaca47f45ea7b7147f1c82afad
Ben Zhou <bz2883@princeton.edu>
2025-04-20 22:29:03 -0400

    Flattened bigintadd.c

---
+++ flattenedbigintadd.c
@@ -0,0 +1,72 @@
+#include "bigint.h"
+#include "bigintprivate.h"
+#include <string.h>
+#include <assert.h>
+
+/* In lieu of a boolean data type. */
+enum {FALSE, TRUE};
+
+/*--------------------------------------------------------------------*/
+
+/* Return the larger of lLength1 and lLength2. */
+
+static long BigInt_larger(long lLength1, long lLength2)
+{
+   long lLarger;
+   if (lLength1 <= lLength2) goto endif1;
+      lLarger = lLength1;
+   else1:
+      lLarger = lLength2;
+    endif1: 
+
+   return lLarger;
+}
+
+/* Assign the sum of oAddend1 and oAddend2 to oSum.  oSum should be
+   distinct from oAddend1 and oAddend2.  Return 0 (FALSE) if an
+   overflow occurred, and 1 (TRUE) otherwise. */
+
+int BigInt_add(BigInt_T oAddend1, BigInt_T oAddend2, BigInt_T oSum)
+{
+    unsigned long ulCarry;
+    unsigned long ulSum;
+    long lIndex;
+    long lSumLength;
+
+    lSumLength = BigInt_larger(oAddend1->lLength, oAddend2->lLength); 
+    if (oSum->lLength <= lSumLength) goto endif1;
+        memset(oSum->aulDigits, 0, MAX_DIGITS * sizeof(unsigned long)); 
+    endif1: 
+
+    ulCarry = 0; 
+    lIndex = 0;
+    loop1:  
+        if (lIndex >= lSumLength) goto endloop1;
+            ulSum = ulCarry; 
+            ulCarry = 0; 
+            ulSum += oAddend1->aulDigits[lIndex]; 
+            if (ulSum >= oAddend1->aulDigits[lIndex]) goto endif1;
+                ulCarry = 1; 
+            endif1: 
+            if (ulSum >= oAddend2->aulDigits[lIndex]) goto endif2;
+                ulCarry = 1; 
+            endif2: 
+            oSum->aulDigits[lIndex] = ulSum; 
+            lIndex++; 
+            goto loop1; 
+    endloop1: 
+
+    if (ulCarry != 1) goto endif3; 
+        if (lSumLength =! MAX_DIGITS) goto endif4; 
+            return FALSE; 
+        endif4: 
+        oSum->aulDigits[lSumLength] = 1; 
+        lSumLength++; 
+    endif3: 
+
+    oSum->lLength = lSumLength; 
+
+    return TRUE; 
+
+
+} 

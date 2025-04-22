fe486b055304f5f3f97481167f5a08832af76538
Owen Clarke <oc8825@princeton.edu>
2025-04-22 10:36:12 -0400

    h file

--- bigintprivate.h
+++ bigintprivate.h
@@ -7,7 +7,10 @@
 #define BIGINTPRIVATE_INCLUDED
 
 /* The maximum number of digits that a BigInt object can contain. */
-enum {MAX_DIGITS = 32768};   /* Arbitrary */
+enum
+{
+   MAX_DIGITS = 32768
+}; /* Arbitrary */
 
 /*--------------------------------------------------------------------*/
 
@@ -19,11 +22,10 @@ struct BigInt
       has length 0. This field could be of type int, but then the
       compiler would place padding between this field and the next. */
    long lLength;
-   
+
    /* The digits comprising the BigInt object. aulDigits[0] stores the
       least significant digit.  The unused digits are set to 0. */
    unsigned long aulDigits[MAX_DIGITS];
 };
 
 #endif
-

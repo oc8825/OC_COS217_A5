fd11ac18eec73531ea79ea09923d5d4cf0e91b0f
Ben Zhou <bz2883@princeton.edu>
2025-04-24 11:38:13 -0400

    attempt

--- bigintaddoptoptoptben.s
+++ bigintaddoptoptoptben.s
@@ -136,8 +136,8 @@ endloop1:
     cbz x10, endif5
 
     // if(lSumLength != MAX_DIGITS) goto endif6
-    mov w6, MAX_DIGITS
-    cmp LSUMLENGTH, w6
+    mov x6, #MAX_DIGITS
+    cmp LSUMLENGTH, x6
     b.ne endif6
 
     // epilog and return
